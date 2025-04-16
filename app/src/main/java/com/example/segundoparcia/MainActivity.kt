6a96aa5624c412d1105e1413cb3ae52b5905426f
Isabella12-bit <isabella.montes@ucb.edu.bo>
2025-04-15 20:22:31 -0400

    UI básica con búsqueda y resultado de libros

--- app/src/main/java/com/example/segundoparcia/MainActivity.kt
+++ app/src/main/java/com/example/segundoparcia/MainActivity.kt
@@ -12,12 +12,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.segundoparcia.ui.theme.SegundoparciaTheme
+import androidx.navigation.compose.rememberNavController
+import com.example.segundoparcia.ui.navigation.AppNavGraph
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
+            AppNavGraph(navController)
             SegundoparciaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
